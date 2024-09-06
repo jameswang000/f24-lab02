853d0815deeea4cdc05738a0ece94e434b237c58
jameswang000 <jameswa2@andrew.cmu.edu>
2024-09-06 11:14:16 -0400

    Documented changes to index.ts for Typescript

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -2,6 +2,12 @@ import { newRenderer } from "./renderer.js"
 import { Shape } from "./shapes/shape.js";
 import { newRectangle } from "./shapes/rectangle.js";
 
+/* 
+ * Modified rectangle variable to be of 
+ * type Shape instead of Rectange
+ * and imports to reflect that.
+ */
+
 const rectangle: Shape = newRectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
