a94dff5c6530aa42dab345c40c2f2f5c4535e421
jameswang000 <jameswa2@andrew.cmu.edu>
2024-09-06 11:16:58 -0400

    Documented changes to main with comments

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -3,6 +3,11 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
 import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
+/* 
+ * Modified rectangle variable to be of 
+ * type Shape instead of Rectange
+ * and imports to reflect that.
+ */
 public class Main {
     public static void main(String[] args) {
         Shape rectangle = new Rectangle(2, 3);
